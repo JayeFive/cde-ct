c0f5e18d7a8b5d6740f4421dfed39310308b4dd5
Johnny Olson <john.olson3@gmail.com>
2018-07-08 19:53:40 +0000

    google maps marker link

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ $(function () {
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
         var marker = new google.maps.Marker({position: location, map: map});
+        
+        marker.addListener('click', function() {
+            window.location.replace("https://www.google.com/maps/place/500+Purdy+Hill+Rd,+Monroe,+CT+06468/@41.3057058,-73.2533479,17z/data=!3m1!4b1!4m5!3m4!1s0x89e8083d181224d1:0x48ce777983cc98cd!8m2!3d41.3057058!4d-73.2511592");
+        });
 
     }
 
